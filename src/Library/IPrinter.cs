0b386918034ed6319b358a26e9555df1b934128b
brunovillero <brunovillero66@gmail.com>
2020-10-25 17:06:44 -0300

    poly and lsp

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -1,5 +1,12 @@
 namespace Full_GRASP_And_SOLID.Library
 {
+    /*
+        Se crea la interfaz IPrinter para asi poder aplicar el patron polymorfismo.
+        Este patron nos permite definir diferentes tipos de recipe printers,
+        cada uno va a definir su forma de imprimir la receta.
+        Podremos imprimir una receta utilizando cualquier clase que implemente
+        esta interfaz IPrinter sin la necesidad de conocer o preguntar por su propio tipo.
+    */
     public interface IPrinter
     {
         void PrintRecipe(Recipe recipe);
